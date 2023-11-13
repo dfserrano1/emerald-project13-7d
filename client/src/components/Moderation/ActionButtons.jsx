d8578a44fb91940e73586f88979191a02984f62e
Retalak <parkerhovis@hotmail.com>
2023-11-13 15:33:55 -0500

    Fixing ActionButtons.jsx duplicates

--- client/src/components/Moderation/ActionButtons.jsx
+++ client/src/components/Moderation/ActionButtons.jsx
@@ -5,14 +5,8 @@ import './ActionButtons.less';
 //  deleteActivity,
 //  getLessonModuleActivities,
 //} from "../../Utils/requests";
-//import { <-- Import database getter/setters necessary for button function
-//  createActivity,
-//  deleteActivity,
-//  getLessonModuleActivities,
-//} from "../../Utils/requests";
 import { Button } from 'antd';
 
-export default function ActionButtons({uniqueKey, display = 0}) {
 export default function ActionButtons({uniqueKey, display = 0}) {
 
   function Approve() {
@@ -20,11 +14,8 @@ export default function ActionButtons({uniqueKey, display = 0}) {
     alert("The content has been approved! Uniqiue key: " + uniqueKey);
     
     // Use setter from requests.js to update status column as approved
-    
-    // Use setter from requests.js to update status column as approved
 
     // Remove hidden status from gallery, if hidden
-
 
   }
 
@@ -33,10 +24,8 @@ export default function ActionButtons({uniqueKey, display = 0}) {
     alert("The content has been rejected! Uniqiue key: " + uniqueKey);
 
     // Use setter from requests.js to update status column as rejected
-    // Use setter from requests.js to update status column as rejected
 
     // Enabling hiding post from gallery if not already hidden
-    
     
   }
 
