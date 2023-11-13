3bf3e88bd40c646d51d5e44a1ce3dfacaf90e591
Liam McKenna <lmcmckenna@gmail.com>
2023-11-13 14:27:33 -0500

    added flag page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import PrivateRoute from './Utils/PrivateRoute';
 import About from './views/About/About';
 import Administrator from './views/Administrator/Administrator';
+import FlagPage from './views/FlagPage/FlagPage';
 import BlocklyPage from './views/BlocklyPage/BlocklyPage';
 import BugReport from './views/BugReport/BugReport';
 import ContentCreator from './views/ContentCreator/ContentCreator';
@@ -34,6 +35,7 @@ const App = () => {
         <Route path='/replay/:saveID' element={<Replay />} />
         <Route path='/sandbox' element={<BlocklyPage isSandbox={true} />} />
         <Route path='/administrator' element={<Administrator />} />
+        <Route path='/flag' element={<FlagPage />} />
         <Route
           path='/report'
           element={
---
+++ client/src/views/FlagPage/FlagPage.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import NavBar from "../../components/NavBar/NavBar"
+import CommentFlagButton from "../../components/Moderation/CommentFlagButton"
+import "./FlagPage.less"
+
+export default function FlagPage(props) {
+    const num = 3;
+    return (
+        <div className="container nav-padding">
+            <NavBar/>
+            <CommentFlagButton uniqueKey={ num } />
+        </div>
+    )
+}
